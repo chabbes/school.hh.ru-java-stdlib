7ceacce02325c7d6e9ef2091307b0b1f6db29a0d
Anton Artemov <chabbes@gmail.com>
2012-01-06 15:08:12 +0400

    commit

--- src/main/java/ru/hh/school/stdlib/Server.java
+++ src/main/java/ru/hh/school/stdlib/Server.java
@@ -1,18 +1,94 @@
 package ru.hh.school.stdlib;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Server {
-  public Server(InetSocketAddress addr) {
-    throw new UnsupportedOperationException();
-  }
+    private Substitutor3000 substitutor;
+    private ServerSocket serverSocket;
+    private long sleep;
 
-  public void run() throws IOException {
-    throw new UnsupportedOperationException();
-  }
+    public Server(InetSocketAddress addr) {
+        try {
+            serverSocket = new ServerSocket(addr.getPort(), 0, addr.getAddress());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        substitutor = new Substitutor3000();
+        sleep = 0;
+    }
 
-  public int getPort() {
-    throw new UnsupportedOperationException();
-  }
+    public void run() throws IOException {
+        Executor e = Executors.newCachedThreadPool();
+        while(true) {
+            final Socket socket = serverSocket.accept();
+            System.out.println("Client connected");
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    BufferedReader in = null;
+                    PrintWriter out = null;
+
+                    String input[];
+
+                    try {
+                        Thread.sleep(sleep);
+
+                        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        out = new PrintWriter(socket.getOutputStream(), true);
+                        input = in.readLine().split(" ");
+
+                        String info = "Command with error! ";
+
+                        if (input[0].equals("GET")) {
+                            if (input.length == 2) {
+                                info = "VALUE\n" + substitutor.get(input[1]) + "\n";
+                            } else {
+                                info += "USAGE: GET key\n";
+                            }
+                        } else if (input[0].equals("PUT")) {
+                            if (input.length == 3) {
+                                // Не используем пробел в качестве разделителя
+                                substitutor.put(input[1], input[2]);
+                                info = "OK\n";
+                            } else {
+                                info += "USAGE: PUT key value\n";
+                            }
+                        } else if (input[0].equals("SET") && input[1].equals("SLEEP")) {
+                            if (input.length == 3) {
+                                sleep = Integer.parseInt(input[2]);
+                                info = "OK\n";
+                            } else {
+                                info += "USAGE: SET SLEEP number\n";
+                            }
+                        }
+
+                        out.write(info);
+                        out.flush();
+
+                        socket.close();
+                        System.out.println("Connection closed");
+                        in.close();
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            e.execute(r);
+        }
+    }
+
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
 }
--- src/main/java/ru/hh/school/stdlib/Substitutor3000.java
+++ src/main/java/ru/hh/school/stdlib/Substitutor3000.java
@@ -1,11 +1,35 @@
 package ru.hh.school.stdlib;
 
-public class Substitutor3000 {
-  public void put(String key, String value) {
-    throw new UnsupportedOperationException();
-  }
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-  public String get(String key) {
-    throw new UnsupportedOperationException();
-  }
+public class Substitutor3000 {
+    private HashMap<String, String> hashMap;
+
+    private static final Pattern pattern = Pattern.compile("\\$\\{(.+?)\\}");
+
+    public Substitutor3000() {
+        hashMap = new HashMap<String, String>();
+    }
+
+    public synchronized void put(String key, String value) {
+        hashMap.put(key, value);
+    }
+
+    public synchronized String get(String key) {
+        if (!hashMap.containsKey(key))
+            return null;
+
+        Matcher matcher = pattern.matcher(hashMap.get(key));
+        while (matcher.find()) {
+            String tok = "";
+            if (hashMap.containsKey(matcher.group(1))) {
+                tok = hashMap.get(matcher.group(1)) + " ";
+            }
+            matcher = pattern.matcher(matcher.replaceFirst(tok));
+        }
+        String result = matcher.replaceFirst("");
+        return result;
+    }
 }
